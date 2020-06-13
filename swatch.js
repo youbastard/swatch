2642f4dfbf12f25e82420a81a7edd2997c5037a7
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2020-06-12 22:49:23 -0400

    reduce some bytes

--- swatch.js
+++ swatch.js
@@ -3,16 +3,14 @@
  * author: nicholas ortenzio
  * inspired by the game "i love hue"
  **/
-// TODO - add win state (in title bar?)
-// TODO - add selected state for rectangle
-// TODO - add fixed state for rectangle
-// DOM ELEMENTS 
-// y=svg
-// z=status
+/**
+ * DOM ELEMENTS 
+ * y=svg
+ * z=status
+ **/
 // map function
 A='map';
 // hex to dec conversion
-// +(0x+b) will cast a hex value b to a decimal
 B=a=>[...a][A](b=>+('0x'+b+b));
 // fill cells array with 36 objects
 C=[];while(C.push({})<36);
@@ -34,7 +32,7 @@ L=a=>(b=C.filter(c=>!c.f)[A](c=>c.i).sort(_=>Math.random()-.5),C[A](c=>c.f||(c.i
 // get level rgb data
 M=a=>Z[A](b=>B(b.substr(K,4)));
 // set status
-N=_=>z[D]=`Level:${K+1} Moves:${J++}`;
+N=_=>z[D]=`Level:${++K} Moves:${J++}`;
 // redraw board - update status then update all rectangles then update x & y
 O=_=>(N(),C[A](({i},b)=>E(y.childNodes[b],{i,x:X(i),y:Y(i)})));
 // initialize level data
@@ -56,27 +54,27 @@ Q=z=>{
     a.f=/^[3]*[50]/.test(b);
     // set the color
     a.fill=`rgb(${M()[A](c=>G(X(b)/5,Y(b)/5,...c))})`;
-    // create a svg rect 
-    let r=document.createElementNS('http://www.w3.org/2000/svg','rect');
+    // create a rect element and append it to svg
+    r=y.appendChild(document.createElementNS('http://www.w3.org/2000/svg','rect'));
     // set the attributes
     E(r,{height:1,width:1,x:X(b),y:Y(b),...a});
     // add onclick handler
     r.onclick=e=>{
       // if a fixed item was clicked or the game has not yet init
-      if (a.f||!I)return zzfx();
+      if(a.f||!I)return R();
       // play click sound
-      zzfx(1);
+      R(1);
       // if 2 items are in the swap, do swap, clear swap, redraw board, play sound effect
-      if (P.push(a)==2)H(...P),P=[],O(),zzfx(2);
+      if(P.push(a)==2)H(...P),P=[],O(R(2));
       // if sorted, start next Level
       F(C)&&Q(++K)
     };
-    // append rect to svg
-    y.appendChild(r);
   });
   // wait 1 seconds, shuffle array elements, call draw function  set is init
-  setTimeout(_=>{L();O();I=1},1e3);
+  setTimeout(_=>{O(L());I=1},1e3);
 }
+// ZZFX
+R=a=>zzfx(a);
 // index to x location
 X=i=>i%6;
 // index to y location
